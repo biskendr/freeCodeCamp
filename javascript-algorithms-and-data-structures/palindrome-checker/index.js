f63b572a0fbc0ef892f5992b347770cc2852382c
Burak İskender <burak.iskendr@gmail.com>
2022-11-14 16:59:29 +0300

    add: palindrome checker function was built

---
+++ javascript-algorithms-and-data-structures/palindrome-checker/index.js
@@ -0,0 +1,13 @@
+function palindrome(str) {
+    let regex = /[a-z0-9]/gi; //Regex  
+    let arr = str.toLowerCase().match(regex);
+    let len = arr.length - 1;
+
+    for (let i = 0; i < arr.length / 2; i++) {
+        if (arr[i] !== arr[len - i])
+            return false;
+    }
+    return true;
+}
+
+palindrome("eybiribye");
